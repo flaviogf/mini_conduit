f3e41eb08bc76e2d08344927e44be108c90dfad1
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:52:55 -0300

    feat: adding User

---
+++ backend/src/Conduit.Api/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Conduit.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonIgnore]
+        public string PasswordHash { get; set; }
+
+        [NotMapped]
+        public string Token { get; set; }
+
+        public string Bio { get; set; }
+
+        public string Image { get; set; }
+    }
+}
