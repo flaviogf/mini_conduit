71507b98b0d91e67ff92ed7c16b9f9a632581015
flaviogf <flavio.fernandes6@gmail.com>
2020-09-05 19:33:54 -0300

    feat: adding ArticleRepository

---
+++ backend/src/Conduit.Api/Repositories/EFArticleRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Conduit.Api.Models;
+
+namespace Conduit.Api.Repositories
+{
+    public class EFArticleRepository : IArticleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EFArticleRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Save(Article article)
+        {
+            await _context.Articles.AddAsync(article);
+        }
+    }
+}
---
+++ backend/src/Conduit.Api/Repositories/IArticleRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Conduit.Api.Models;
+
+namespace Conduit.Api.Repositories
+{
+    public interface IArticleRepository
+    {
+        Task Save(Article article);
+    }
+}
