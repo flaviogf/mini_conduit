1b0fdae3a3f3deb13f73690b45902fb3f723e95c
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:55:07 -0300

    feat: adding NewUserResponse

---
+++ backend/src/Conduit.Api/ViewModels/NewUserResponse.cs
@@ -0,0 +1,14 @@
+﻿using Conduit.Api.Models;
+
+namespace Conduit.Api.ViewModels
+{
+    public class NewUserResponse
+    {
+        public NewUserResponse(User user)
+        {
+            User = user;
+        }
+
+        public User User { get; }
+    }
+}
