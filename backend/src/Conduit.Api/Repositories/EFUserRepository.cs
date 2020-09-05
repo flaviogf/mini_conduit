397d00ddd9ab46ea8bd3783f3ad76fabba093447
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:54:28 -0300

    feat: adding UserRepository

---
+++ backend/src/Conduit.Api/Repositories/EFUserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Conduit.Api.Models;
+using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conduit.Api.Repositories
+{
+    public class EFUserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EFUserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Save(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+        public async Task<Maybe<User>> FindByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(it => it.Email == email);
+        }
+    }
+}
---
+++ backend/src/Conduit.Api/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Conduit.Api.Models;
+using CSharpFunctionalExtensions;
+
+namespace Conduit.Api.Repositories
+{
+    public interface IUserRepository
+    {
+        Task Save(User user);
+
+        Task<Maybe<User>> FindByEmail(string email);
+    }
+}
