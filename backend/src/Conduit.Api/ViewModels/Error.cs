5b340a1d7772672c9de4f39d7b9576eb59555bb3
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:54:51 -0300

    feat: adding ErrorResponse

---
+++ backend/src/Conduit.Api/ViewModels/Error.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Conduit.Api.ViewModels
+{
+    public class Error
+    {
+        public Error(IEnumerable<string> body)
+        {
+            Body = body;
+        }
+
+        public IEnumerable<string> Body { get; }
+    }
+}
---
+++ backend/src/Conduit.Api/ViewModels/ErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Conduit.Api.ViewModels
+{
+    public class ErrorResponse
+    {
+        public ErrorResponse(params string[] messages)
+        {
+            Errors = new Error(messages);
+        }
+
+        public Error Errors { get; }
+    }
+}
