0202e981a4180abd2b8c7a228c0e41725dde8c99
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:53:10 -0300

    feat: adding Bcrypt hash

---
+++ backend/src/Conduit.Api/Infrastructure/Bcrypt.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Conduit.Api.Infrastructure
+{
+    public class Bcrypt : IHash
+    {
+        public Task<string> Make(string value)
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword(value);
+
+            return Task.FromResult(hash);
+        }
+    }
+}
---
+++ backend/src/Conduit.Api/Infrastructure/IHash.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Conduit.Api.Infrastructure
+{
+    public interface IHash
+    {
+        Task<string> Make(string value);
+    }
+}
