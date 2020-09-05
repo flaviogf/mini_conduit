dcda1061c376b6e114dbe8f78fc4061f2a31a339
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:56:38 -0300

    feat: adding Startup and Program class

---
+++ backend/src/Conduit.Api/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using Conduit.Api.Infrastructure;
+using Conduit.Api.Repositories;
+using Conduit.Api.ViewModels;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Conduit.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(it => it.UseSqlite(_configuration.GetConnectionString("ApplicationDbContext")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IHash, Bcrypt>();
+
+            services.AddScoped<IUserRepository, EFUserRepository>();
+
+            services
+                .AddControllers()
+                .ConfigureApiBehaviorOptions(it =>
+                {
+                    it.InvalidModelStateResponseFactory = (context) =>
+                    {
+                        string[] message = context.ModelState.Select(it => it.Value).SelectMany(it => it.Errors).Select(it => it.ErrorMessage).ToArray();
+
+                        var result = new ObjectResult(new ErrorResponse(message))
+                        {
+                            StatusCode = 402
+                        };
+
+                        return result;
+                    };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
