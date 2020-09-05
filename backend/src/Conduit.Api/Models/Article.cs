27de144cad673194c5ba92e1bafe5a691b599e64
flaviogf <flavio.fernandes6@gmail.com>
2020-09-05 19:04:58 -0300

    feat: adding Article

---
+++ backend/src/Conduit.Api/Models/Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Conduit.Api.Models
+{
+    public class Article
+    {
+        public string Slug { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Body { get; set; }
+
+        public IEnumerable<string> TagList { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public bool Favorited { get; set; }
+
+        public int FavoritesCount { get; set; }
+
+        public User Author { get; set; }
+    }
+}
