3056c78b73fd47a2fa9cbdfcb56ec42d61e1255f
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:52:22 -0300

    feat: adding NewUser and NewUserRequest

---
+++ backend/src/Conduit.Api/ViewModels/NewUserRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conduit.Api.ViewModels
+{
+    public class NewUserRequest
+    {
+        [Required]
+        public NewUser User { get; set; }
+    }
+}
