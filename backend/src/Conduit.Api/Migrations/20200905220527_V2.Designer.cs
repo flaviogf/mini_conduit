ccf07679e07175366cea2a5a58b083252297ba90
flaviogf <flavio.fernandes6@gmail.com>
2020-09-05 19:33:09 -0300

    feat: adding new migration

--- backend/src/Conduit.Api/Migrations/20200905220527_V2.Designer.cs
+++ backend/src/Conduit.Api/Migrations/20200905220527_V2.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Conduit.Api;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Conduit.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200905220527_V2")]
+    partial class V2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
