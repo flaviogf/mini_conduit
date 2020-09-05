e6223f4301c50c4a85ff5a50b684814613d2fc66
flaviogf <flavio.fernandes6@gmail.com>
2020-09-04 21:54:03 -0300

    feat: adding ApplicationContext

---
+++ backend/src/Conduit.Api/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Conduit.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conduit.Api
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
