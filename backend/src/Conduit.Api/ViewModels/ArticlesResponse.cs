64aa72b7abb85aff3ece7e29c12d5f20030ca281
flaviogf <flavio.fernandes6@gmail.com>
2020-09-05 20:25:19 -0300

    feat: adding ArticlesResponse

---
+++ backend/src/Conduit.Api/ViewModels/ArticlesResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Conduit.Api.Models;
+
+namespace Conduit.Api.ViewModels
+{
+    public class ArticlesResponse
+    {
+        public ArticlesResponse(IEnumerable<Article> articles)
+        {
+            Articles = articles;
+        }
+
+        public IEnumerable<Article> Articles { get; }
+    }
+}
