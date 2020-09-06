d18a5e8693264b9636b228df820ea683b9d66421
flaviogf <flavio.fernandes6@gmail.com>
2020-09-06 18:49:03 -0300

    feat: adding ArticlesFilter

---
+++ backend/src/Conduit.Api/ViewModels/ArticlesFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace Conduit.Api.ViewModels
+{
+    public class ArticlesFilter
+    {
+        public string Tag { get; set; }
+
+        public string Author { get; set; }
+
+        public string Favorited { get; set; }
+    }
+}
