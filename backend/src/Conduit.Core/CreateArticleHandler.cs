2009ad1551585d7c30d43c0f502635de0ba5cb04
flaviogf <flavio.fernandes6@gmail.com>
2020-09-08 21:48:06 -0300

    feat: adding CreateArticleHandler

---
+++ backend/src/Conduit.Core/CreateArticleHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace Conduit.Core
+{
+    class CreateArticleHandler : IRequestHandler<CreateArticleRequest, Result>
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public CreateArticleHandler(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<Result> Handle(CreateArticleRequest request, CancellationToken cancellationToken)
+        {
+            var article = new Article(request.Title, request.Description, request.Body);
+
+            article.AddTags(request.TagList);
+
+            await _articleRepository.Add(article);
+
+            return Result.Success();
+        }
+    }
+}
