00ed5d9d190ebb2ffd4579322ddb8bfdf1c32a3b
flaviogf <flavio.fernandes6@gmail.com>
2020-09-08 21:42:30 -0300

    feat: adding CreateArticleRequest

---
+++ backend/src/Conduit.Core/CreateArticleRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace Conduit.Core
+{
+    public sealed class CreateArticleRequest : IRequest<Result>
+    {
+        public CreateArticleRequest(string title, string description, string body, IEnumerable<string> tagList)
+        {
+            Title = title;
+            Description = description;
+            Body = body;
+            TagList = tagList;
+        }
+
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public string Body { get; }
+
+        public IEnumerable<string> TagList { get; }
+    }
+}
