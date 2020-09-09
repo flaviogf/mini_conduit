2a2b45dbd31ed29398a91c6f334650cc2338c901
flaviogf <flavio.fernandes6@gmail.com>
2020-09-09 11:51:01 -0300

    feat: implementing ToString and implicit operator string in Tag

--- backend/src/Conduit.Core/Tag.cs
+++ backend/src/Conduit.Core/Tag.cs
@@ -8,5 +8,15 @@
         }
 
         public string Name { get; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static implicit operator string(Tag tag)
+        {
+            return tag.Name;
+        }
     }
 }
