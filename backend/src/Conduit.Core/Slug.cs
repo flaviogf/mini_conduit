1cd3b66301fd34f557640568702a4b3908948a57
flaviogf <flavio.fernandes6@gmail.com>
2020-09-08 21:47:48 -0300

    feat: adding Slug

---
+++ backend/src/Conduit.Core/Slug.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Conduit.Core
+{
+    public class Slug
+    {
+        public Slug(string value)
+        {
+            Value = Slugify(value);
+        }
+
+        public string Value { get; }
+
+        private string Slugify(string value) => Regex.Replace(value, @"\s", "-").ToLower();
+    }
+}
