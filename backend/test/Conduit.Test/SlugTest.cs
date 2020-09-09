9f3cec565f83dbd71b7bc32bf24579e54cea7c30
flaviogf <flavio.fernandes6@gmail.com>
2020-09-09 11:39:46 -0300

    test: adding SlugTest

---
+++ backend/test/Conduit.Test/SlugTest.cs
@@ -0,0 +1,32 @@
+﻿using Conduit.Core;
+using Xunit;
+
+namespace Conduit.Test
+{
+    public class SlugTest
+    {
+        [Fact]
+        public void ValueShouldReturnThePassedValueSlugified()
+        {
+            var slug = new Slug("How to train your dragon");
+
+            Assert.Equal("how-to-train-your-dragon", slug.Value);
+        }
+
+        [Fact]
+        public void ToStringShouldReturnTheValueOfSlug()
+        {
+            var slug = new Slug("How to train your dragon");
+
+            Assert.Equal("how-to-train-your-dragon", slug.ToString());
+        }
+
+        [Fact]
+        public void CastToStringShouldReturnTheValueOfSlug()
+        {
+            var slug = new Slug("How to train your dragon");
+
+            Assert.Equal("how-to-train-your-dragon", slug);
+        }
+    }
+}
