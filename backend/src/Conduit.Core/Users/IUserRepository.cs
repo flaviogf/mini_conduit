cc98ca653531e2e4be766c44b4937f2522a29f32
flaviogf <flavio.fernandes6@gmail.com>
2020-09-10 22:53:23 -0300

    feat: adding IUserRepository

---
+++ backend/src/Conduit.Core/Users/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+
+namespace Conduit.Core.Users
+{
+    public interface IUserRepository
+    {
+        Task Add(User user, string password);
+
+        Task<Maybe<User>> FindByEmail(string email);
+    }
+}
