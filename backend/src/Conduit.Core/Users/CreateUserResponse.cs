51efeedb09a6fe302cf42c1410c43dcbd01c4d1e
flaviogf <flavio.fernandes6@gmail.com>
2020-09-10 22:53:01 -0300

    feat: adding CreateUserResponse

---
+++ backend/src/Conduit.Core/Users/CreateUserResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Conduit.Core.Users
+{
+    public class CreateUserResponse
+    {
+        private readonly User _user;
+
+        internal CreateUserResponse(User user, string token)
+        {
+            _user = user;
+            Token = token;
+        }
+
+        public string Username => _user.Username;
+
+        public string Email => _user.Email;
+
+        public string Token { get; }
+
+        public string Bio => _user.Bio;
+
+        public string Image => _user.Image;
+    }
+}
