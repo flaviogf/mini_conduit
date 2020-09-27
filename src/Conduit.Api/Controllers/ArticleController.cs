962df25b237e2e2b47e42529a37f5d863b3519ae
flaviogf <flavio.fernandes6@gmail.com>
2020-09-27 20:38:52 -0300

    feat: adding ArticleController

---
+++ src/Conduit.Api/Controllers/ArticleController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Conduit.Api.Database;
+using Conduit.Api.Models;
+using Conduit.Api.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Res = Conduit.Api.ViewModels.Response;
+
+namespace Conduit.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/article")]
+    public class ArticleController : ControllerBase
+    {
+        private readonly ConduitDbContext _context;
+
+        private readonly UserManager<User> _userManager;
+
+        public ArticleController(ConduitDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] NewArticleRequest req)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var article = new Article
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = req.Title,
+                Description = req.Description,
+                Body = req.Body,
+                AuthorId = user.Id
+            };
+
+            var tags = await GetOrCreateTags(req.Tags);
+
+            article.AddTags(tags);
+
+            await _context.Articles.AddAsync(article);
+
+            await _context.SaveChangesAsync();
+
+            return Created($"api/article/{article.Id}", Res.Success(article.Id));
+        }
+
+        private async Task<IEnumerable<Tag>> GetOrCreateTags(IEnumerable<string> tags)
+        {
+            var tasks = tags.Select(GetOrCreateTag);
+
+            return await Task.WhenAll(tasks);
+        }
+
+        private async Task<Tag> GetOrCreateTag(string tagName)
+        {
+            var tag = await _context.Tags.FirstOrDefaultAsync(it => it.Name == tagName);
+
+            return tag ?? new Tag { Id = Guid.NewGuid().ToString(), Name = tagName };
+        }
+    }
+}
