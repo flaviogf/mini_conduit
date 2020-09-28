79de33cc84ad8cd85e1d2d2f0431ab5b4c93ee41
flaviogf <flavio.fernandes6@gmail.com>
2020-09-28 20:53:31 -0300

    feat: adding Equals and HashCode

--- src/Conduit.Api/Models/Tag.cs
+++ src/Conduit.Api/Models/Tag.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Conduit.Api.Models
@@ -9,5 +10,15 @@ namespace Conduit.Api.Models
         public string Name { get; set; }
 
         public IList<ArticleTag> Articles { get; set; } = new List<ArticleTag>();
+
+        public override bool Equals(object obj)
+        {
+            return obj is Tag tag && Id == tag.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
