7012b51a63ec1913f1bd20c3bef118e59620d57b
flaviogf <flavio.fernandes6@gmail.com>
2020-09-28 11:05:54 -0300

    feat: adding tag controller

---
+++ src/Conduit.Api/Controllers/TagController.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Conduit.Api.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Res = Conduit.Api.ViewModels.Response;
+
+namespace Conduit.Api.Controllers
+{
+    [ApiController]
+    [Route("api/tag")]
+    public class TagController : ControllerBase
+    {
+        private readonly ConduitDbContext _context;
+
+        public TagController(ConduitDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var tags = await _context.Tags
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(Res.Success(tags));
+        }
+    }
+}
