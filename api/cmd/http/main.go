88373dc11822d9ff579acbb7f6ef599cc47ce683
flaviogf <flavio.fernandes6@gmail.com>
2021-04-25 13:32:38 -0300

    feat: using routes

--- api/cmd/http/main.go
+++ api/cmd/http/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/flaviogf/conduit/api/internal/models"
+	"github.com/flaviogf/conduit/api/internal/routes"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/gorilla/mux"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	http.Handle("/", middleware.SwaggerUI(middleware.SwaggerUIOpts{Path: "/", SpecURL: "swagger.yaml"}, nil))
+	db, err := sql.Open("postgres", os.Getenv("CONDUIT_DB"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	models.Conn = db
+
+	r := mux.NewRouter()
+
+	http.Handle("/", routes.NewHandler(r))
+
+	http.Handle("/docs", middleware.SwaggerUI(middleware.SwaggerUIOpts{SpecURL: "swagger.yaml"}, nil))
 
 	http.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
