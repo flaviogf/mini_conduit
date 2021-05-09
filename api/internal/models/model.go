46580300e72f168ccc53b030311323680d76a074
flaviogf <flavio.fernandes6@gmail.com>
2021-05-09 12:32:04 -0300

    feat: adding QueryContext method

--- api/internal/models/model.go
+++ api/internal/models/model.go
@@ -10,6 +10,7 @@ var Conn *sql.DB
 type Tx interface {
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 	Commit() error
 	Rollback() error
 }
