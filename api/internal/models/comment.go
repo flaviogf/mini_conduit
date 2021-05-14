9b0814bb66330b11ba3545c1123701b079a4daca
flaviogf <flavio.fernandes6@gmail.com>
2021-05-13 21:02:27 -0300

    feat: adding method GetAuthor

--- api/internal/models/comment.go
+++ api/internal/models/comment.go
@@ -1,12 +1,16 @@
 package models
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Comment struct {
-	ID        int64
-	Body      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID          int64
+	Body        string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	commenterId int64
 }
 
 func NewComment(id int64, body string, createdAt, updatedAt time.Time) *Comment {
@@ -16,4 +20,22 @@ func NewComment(id int64, body string, createdAt, updatedAt time.Time) *Comment 
 		CreatedAt: createdAt,
 		UpdatedAt: updatedAt,
 	}
+}
+
+func (c Comment) GetAuthor(ctx context.Context) (User, error) {
+	sql := `SELECT id, username, email, password_hash, bio, image FROM users WHERE id = $1`
+
+	row := ctx.Value("tx").(Tx).QueryRowContext(ctx, sql, c.commenterId)
+
+	if err := row.Err(); err != nil {
+		return User{}, err
+	}
+
+	var user User
+
+	if err := row.Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash, &user.Bio, &user.Image); err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
